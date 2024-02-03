b12347e4fb079cc05a28fd0e76b0398a676b4ff5
Urooj-Abid <s.urooj.abid@gmail.com>
2024-02-03 14:54:31 +0500

    first commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+my blogging about
+cooking
+gardening
+home decor
+organization
+task management
+parenting
+fashion and styling
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => (
+  <>
+  <div className=' text-red-800 w-full bg-black'>i m new</div> 
+  </>
+  
+);
+
+export default App
